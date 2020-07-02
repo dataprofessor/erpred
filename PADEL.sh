d427b1d395bf4decd68f5a0943282a477b6e6221
Chanin Nantasenamat <chaninn@users.noreply.github.com>
2020-07-02 14:35:40 +0700

    Update PADEL.sh

--- PADEL.sh
+++ PADEL.sh
@@ -1,1 +1,1 @@
-java -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/SubstructureFingerprinter.xml -dir ./ -file descriptors_output.csv
+java -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv
